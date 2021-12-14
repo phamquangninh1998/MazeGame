e3fa8dd9f5818e88ffbbd37ac972a98bcf04f294
ninhninh <65784967+phamquangninh1998@users.noreply.github.com>
2021-12-14 18:02:32 +0700

    +add game

---
+++ Maze/Assets/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitive = 100;
+    public float horizontalMouseSensitive = 2;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitive * Time.deltaTime * horizontalMouseSensitive ;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitive * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
