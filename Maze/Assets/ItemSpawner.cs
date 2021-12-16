f5bda96db6de64fc4f7a1270757fc1af5a8512bd
ninhninh <65784967+phamquangninh1998@users.noreply.github.com>
2021-12-16 15:41:14 +0700

    +add itemspawner

---
+++ Maze/Assets/ItemSpawner.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+
+    public int numberOfItemToSpawn = 3;
+    public int roomCount;
+    public Item itemPrefab;
+    public Transform rooms;
+    public List<Color> colorList;
+
+    private void Start()
+    {
+        roomCount = rooms.childCount;
+        SpawnItemToRoom();
+    }
+
+    private void SpawnItemToRoom()
+    {
+        int lastRoomId = -1;
+        for (int i = 0; i < numberOfItemToSpawn; i++)
+        {
+            Item newItem = Instantiate(itemPrefab);
+
+            int newRandomColorIndex = Random.Range(0, colorList.Count);
+            Color randomColor = colorList[newRandomColorIndex];
+            colorList.RemoveAt(newRandomColorIndex);
+
+            newItem.GetComponent<Renderer>().material.color = randomColor;
+
+
+            int newRoomId = Random.Range(lastRoomId + 1, roomCount - (numberOfItemToSpawn - i));
+
+            lastRoomId = newRoomId;
+
+            RoomController selectedRoom = rooms.GetChild(newRoomId).GetComponent<RoomController>();
+
+            selectedRoom.SetItem(newItem);
+        }
+    }
+}
---
+++ Maze/Assets/RoomController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public int id;
+    public GameObject door;
+    public Transform itemPosition;
+
+    private void Awake()
+    {
+        id = transform.GetSiblingIndex();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CloseDoor();
+        }
+    }
+
+    private void CloseDoor()
+    {
+        door.SetActive(true);
+    }
+
+    private void OpenDoor()
+    {
+        door.SetActive(false);
+    }
+
+    internal void SetItem(Item newItem)
+    {
+        newItem.transform.position = itemPosition.position;
+        newItem.roomId = id;
+    }
+}
